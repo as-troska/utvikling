ba72e64c1d38b864451bff862a39ded3286bb08c
as-troska <troska@asvg.no>
2022-11-07 14:20:57 +0100

    La til oblig1

---
+++ oblig/oblig1.js
@@ -0,0 +1,21 @@
+//Oppgave 1
+let utskriftOppg1 = document.getElementById("utskriftOppg1");
+
+
+
+//Oppgave 2
+let utskriftOppg2 = document.querySelector("");
+
+//Oppgave 3
+
+let knappOppg3 = document.querySelector("#btnOppg3")
+knappOppg3.addEventListener("click", oppg3)
+
+function oppg3() {
+
+}
+
+//Oppgave 4
+
+
+//Oppgave 5
